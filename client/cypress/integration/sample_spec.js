64d3c1a4975c2e1a688cd63cedb0ab608b8d8cbb
Tim Corley <tcorley215@gmail.com>
2021-05-03 16:14:17 -0400

    adding login tests

--- client/cypress/integration/sample_spec.js
+++ client/cypress/integration/sample_spec.js
@@ -1,11 +1,55 @@
-describe("My First Test", () => {
-  it("Invalid Credentials Prompt Error", () => {
+describe("Login Tests", () => {
+  it("Login Button Directs to Login Page", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Login").click();
+    cy.url().should("include", "/login");
+    cy.get(".form > h1").should("have.text", "Login");
+  });
+  it("Invalid Credentials Prompts Error", () => {
     cy.visit("http://localhost:3000/login");
-    // cy.contains("Login").click();
-    // cy.url().should("include", "/login");
-    cy.get('input[name="username"]').type("fake@email.com");
+    cy.get('input[name="username"]').type("Not User");
     cy.get('input[name="password"]').type("wrongpassword");
     cy.get('button[id="login-btn"]').click();
-    cy.get("[id=error-block]").should("be.visible");
+    cy.get('[id="error-block"]').should("be.visible");
+    cy.get('[id="error-block"]').should("have.text", "User not found");
+  });
+  it("Blank Password Field Promts Error", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[name="username"]').type("Not User");
+    cy.get('button[id="login-btn"]').click();
+    cy.get('[id="error-block"]').should("be.visible");
+    cy.get('[id="error-block"]').should(
+      "have.text",
+      "password cannot be blank"
+    );
+  });
+  it("Blank Username Field Promts Error", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[name="password"]').type("wrongpassword");
+    cy.get('button[id="login-btn"]').click();
+    cy.get('[id="error-block"]').should("be.visible");
+    cy.get('[id="error-block"]').should(
+      "have.text",
+      "username cannot be blank"
+    );
+  });
+  it("Login Works with Valid Credentials", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[name="username"]').type(Cypress.env("VALID_USERNAME"));
+    cy.get('input[name="password"]').type(Cypress.env("VALID_PASSWORD"));
+    cy.get('button[id="login-btn"]').click();
+    cy.get('[id="logout-btn"]').should("be.visible");
+    cy.get(".page-title").should("have.text", "Recent Posts");
+    cy.get(".form > h2").should("have.text", "Create New Post");
+    cy.get('[id="username"]').should("have.text", "Demo User");
+  });
+  it("User Can Logout", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[name="username"]').type(Cypress.env("VALID_USERNAME"));
+    cy.get('input[name="password"]').type(Cypress.env("VALID_PASSWORD"));
+    cy.get('button[id="login-btn"]').click();
+    cy.get('[id="username"]').should("have.text", "Demo User");
+    cy.get('[id="logout-btn"]').click();
+    cy.get('[id="home-btn"]').should("be.visible");
   });
 });
